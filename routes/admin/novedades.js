3e7fab4de884f664a5fcc9f342122d2ebb392949
Jorge Rivas <cabeza822001@gmail.com>
2021-12-11 02:11:54 -0300

    subiendo luna

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,40 @@
+const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
 var novedadesModel= require('../../models/novedadesModel');
+var util= require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader= util.promisify(cloudinary.uploader.upload);
+const destroy= util.promisify(cloudinary.uploader.destroy);
+
+cloudinary.config({ 
+    cloud_name: 'dddwvby4k', 
+    api_key: '433614177672923', 
+    api_secret: 'QO0hvOwpsoqebFzBUsDCZlIE_Bg' 
+  });
 
 router.get('/', async function (req, res, next) {
 
     var novedades= await novedadesModel.getNovedades();
+
+    novedades= novedades.map(novedad=>{
+        if(novedad.img_id){
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 100,
+                height: 100, 
+                crop: 'fill'
+            });
+            return{
+                ...novedad,
+                imagen
+            }
+        }else{
+            return{
+                ...novedad,
+                imagen: ''
+            }
+        }
+    })
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -22,8 +52,20 @@ router.get('/agregar',(req,res,next)=>{
 
 router.post('/agregar', async(req,res,next)=>{
     try{
+
+        var img_id= "";
+        if(req.files && Object.keys(req.files).length>0){
+            imagen= req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
         if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
-            await novedadesModel.insertarNovedades(req.body);
+            await novedadesModel.insertarNovedades({
+                ...req.body,
+                img_id
+            });
+                
+
             res.redirect('/admin/novedades')
         }else{
             res.render('admin/agregar', {
@@ -40,6 +82,67 @@ router.post('/agregar', async(req,res,next)=>{
             message:'No se cargo la novedad'
         })
     }
-})
+});
+
+router.get('/eliminar/:id', async (req,res,next) =>{
+    var id =req.params.id;
+
+    let novedad=await novedadesModel.getNovedadesById(id);
+    if(novedad.img_id){
+        await (destroy(novedad.img_id));
+    }
+
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades');
+});
+
+router.get('/modificar/:id', async (req,res,next) =>{
+    var id =req.params.id;
+    console.log(req.params.id);
+    var novedad= await novedadesModel.getNovedadesById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+});    
+
+router.post('/modificar', async(req,res,next)=>{
+    try{
+        let img_id= req.body.img_original;
+        let borrar_img_vieja= false;
+
+        if(req.body.img_delete === "1"){
+            img_id= null;
+            borrar_img_vieja= true;
+        }else{
+            if(req.files && Object.keys(req.files).length>0){
+                imagen= req.files.imagen;
+                img_id= (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja= true;
+            }
+        }
+        if(borrar_img_vieja && req.body.img_original){
+            await(destroy(req.body.img_original));
+        }
+
+
+        var obj= {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo,
+            img_id
+        }
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    }catch(error){
+        console.log(error);
+        res.render('admin/modificar', {
+            layout:'admin/layout',
+            error:true,
+            message: 'No se cargo la novedad'
+        })
+    }
+});
 
 module.exports = router;
